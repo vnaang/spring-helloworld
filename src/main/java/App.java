d49497adcf8d8fcd22d332aea86738490d4aa1b6
vnaang <anglvd84@gmail.com>
2020-11-20 21:12:12 +0700

    git

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getMessage());
+        System.out.println(bean.equals(bean2));
+        System.out.println(catBean.equals(catBean2));
     }
 }
